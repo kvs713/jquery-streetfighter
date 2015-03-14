e05294ff03152afe317d25b5a58284b01f1c85f9
kvs713 <kvs713@gmail.com>
2015-03-14 13:20:34 -0500

    updated js

--- js/app.js
+++ js/app.js
@@ -1,51 +1,62 @@
 $(document).ready(function() {
-		
+  playGame();
+
+  playTheme();
+});
+
+function playGame() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-still').show();
   })
   .mousedown(function() {
-  	playHadouken();
+    playHadouken();
+    $('.ryu-still').hide();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
       .animate(
-  		{'left': '1020px'},
-  		500,
-  		function() {
-    		$(this).hide();
-    		$(this).css('left', '520px');
-  		}
-	);
+        {'left': '1020px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css('left', '520px');
+        });
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
     // ryu goes back to his ready position
   });
+
   $(document).keydown(function(e) {
     if (e.keyCode == 88) {
       playCool();
-      $('.ryu-standing-still').hide();
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
       $('.ryu-cool').show();
-    }   
+    }
   }).keyup(function(e) {
     if (e.keyCode == 88) {
-	  $('#cool')[0].pause();
-      $('#cool')[0].load();
+	    $('#cool')[0].pause();
+        $('#cool')[0].load();
       $('.ryu-cool').hide();
-      $('.ryu-standing-still').show();
+      $('.ryu-still').show();
     }
   });
-});
+}
 
 function playTheme () {
   $('#theme-song')[0].volume = 0.5;
-  $('#theme-song')[0].load();
+  // $('#theme-song')[0].load();
   $('#theme-song')[0].play();
 }
 
@@ -57,7 +68,6 @@ function playHadouken () {
 
 function playCool () {
   $('#cool')[0].volume = 0.5;
-  $('#cool')[0].load();
+  //$('#cool')[0].load();
   $('#cool')[0].play();
 }
-
